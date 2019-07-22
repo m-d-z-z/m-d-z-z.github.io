6b682603bfe87bc13e50b986e446f7ed398d0321
m-d-z-z <48917995+m-d-z-z@users.noreply.github.com>
2019-07-23 00:57:27 +0800

    Create install.sh

---
+++ shells/ezssr/install.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+configId=$1
+accessKey=$2
+if ((configId > 0)); then
+  echo "running"
+else
+  echo "Missing configId"
+  exit
+fi
+docker version > /dev/null || curl -fsSL get.docker.com | bash
+docker run -dit --network=host --log-opt max-size=50m --log-opt max-file=3 --restart=always nohair/ezssr ezssr $configId $accessKey
+systemctl disable firewalld
+systemctl stop firewalld
