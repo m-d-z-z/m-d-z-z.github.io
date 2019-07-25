2426c058d669ee788be46117eda940c4443fb89a
m-d-z-z <48917995+m-d-z-z@users.noreply.github.com>
2019-07-25 11:21:55 +0800

    Update run.sh

--- shells/ezv2ray/run.sh
+++ shells/ezv2ray/run.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
-v2ray_realpath=$(readlink -f v2ray)
-cfg_realpath=$(readlink -f cfg.json)
-v2ray_pid=$(ps ux | grep "$(readlink -f v2ray)" | grep -v grep | awk '{print $2}')
-if [ ! $v2ray_pid ];
-then
-    echo 'Starting V2Ray'
-else
-    echo 'Restarting V2Ray (pid:'$v2ray_pid')'
-    kill -9 $v2ray_pid
-fi
 configId=$1
 accessKey=$2
-
-nohup $(readlink -f v2ray) --config=https://www.x-air.icu/api/hostedConfig/$configId/\?key=$accessKey >> /dev/null 2>&1 &
-echo 'Preparing...'
-sleep 1
-
-v2ray_pid=$(ps ux | grep "$(readlink -f v2ray)" | grep -v grep | awk '{print $2}')
-
-if [ ! $v2ray_pid ];
-then
-    echo -e "\033[31m***Fail to start V2Ray***\033[0m"
+checkFlag=$3
+v2ray_realpath=$(readlink -f v2ray)
+cfg_realpath=$(readlink -f cfg.json)
+v2ray_pid=$(ps ux | grep "$(readlink -f v2ray) --config=https://www.x-air.icu/api/hostedConfig/$configId/?key=$accessKey" | grep -v grep | awk '{print $2}')
+v2rayFile="./v2ray"
+v2ctlFile="./v2ctl"
+if [ ! -e $v2rayFile || ! -e $v2ctlFile ]; then
+    echo '错误： 本脚本依赖的核心文件 v2ray 和 v2ctl 不存在'
+    exit
+fi
+if [ ! -x $v2rayFile || ! -x $v2ctlFile ]; then
+    echo '错误：本脚本依赖的核心文件 v2ray 和 v2ctl 没有运行权限'
+    exit
+fi
+if [ ! $v2ray_pid ]; then
+    echo '正在启动 V2Ray'
+    nohup $(readlink -f v2ray) --config=https://www.x-air.icu/api/hostedConfig/$configId/?key=$accessKey >>/dev/null 2>&1 &
 else
-    echo -e "\033[32mSuccess to start V2Ray (pid:'$v2ray_pid')\033[0m"
+    if [ $flag ]; then
+        echo "V2ray 已经运行 (pid: $v2ray_pid)，请享用～."
+    else
+        echo '正在重启 V2Ray (pid:'$v2ray_pid')'
+        kill -9 $v2ray_pid
+        nohup $(readlink -f v2ray) --config=https://www.x-air.icu/api/hostedConfig/$configId/?key=$accessKey >>/dev/null 2>&1 &
+        echo 'Preparing...'
+        sleep 1
+
+        v2ray_pid=$(ps ux | grep "$(readlink -f v2ray)" | grep -v grep | awk '{print $2}')
+
+        if [ ! $v2ray_pid ]; then
+            echo -e "\033[31m***V2ray重启失败，可能是配置文件出错或者端口被占用，请检查***\033[0m"
+        else
+            echo -e "\033[32mV2ray启动成功 (pid:'$v2ray_pid')\033[0m"
+        fi
+    fi
 fi
