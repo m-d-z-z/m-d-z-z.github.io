4e829dbcd8c0238a5963ade4b5df49e14b8c9cee
m-d-z-z <48917995+m-d-z-z@users.noreply.github.com>
2019-08-06 18:55:34 +0800

    Update run.sh

--- shells/ezv2ray/run.sh
+++ shells/ezv2ray/run.sh
@@ -18,6 +18,16 @@ fi
 if [ ! $v2ray_pid ]; then
     echo '正在启动 V2Ray'
     nohup $(readlink -f v2ray) --config=https://www.x-air.icu/api/hostedConfig/$configId/?key=$accessKey >>/dev/null 2>&1 &
+    echo 'Preparing...'
+        sleep 3
+
+        v2ray_pid=$(ps ux | grep "$(readlink -f v2ray)" | grep -v grep | awk '{print $2}')
+
+        if [ ! $v2ray_pid ]; then
+            echo -e "\033[31m***V2ray重启失败，可能是配置文件出错或者端口被占用，请检查***\033[0m"
+        else
+            echo -e "\033[32mV2ray启动成功 (pid:'$v2ray_pid')\033[0m"
+        fi
 else
     if [ $flag ]; then
         echo "V2ray 已经运行 (pid: $v2ray_pid)，请享用～."
@@ -26,7 +36,7 @@ else
         kill -9 $v2ray_pid
         nohup $(readlink -f v2ray) --config=https://www.x-air.icu/api/hostedConfig/$configId/?key=$accessKey >>/dev/null 2>&1 &
         echo 'Preparing...'
-        sleep 1
+        sleep 3
 
         v2ray_pid=$(ps ux | grep "$(readlink -f v2ray)" | grep -v grep | awk '{print $2}')
 
