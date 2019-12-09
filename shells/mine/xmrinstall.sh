2c9b3d3f186b4f44b7888ad41c7abc2dd9bc2a0e
m-d-z-z <48917995+m-d-z-z@users.noreply.github.com>
2019-12-09 21:22:03 +0800

    Create xmrinstall.sh

---
+++ shells/mine/xmrinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get -y install aptitude
+sudo apt update
+sudo apt install -y wget build-essential autotools-dev autoconf automake libcurl3 libcurl4-gnutls-dev git make cmake libssl-dev pkg-config libevent-dev libunbound-dev libminiupnpc-dev doxygen supervisor jq libboost-all-dev htop libmicrohttpd-dev
+sudo git clone https://github.com/fireice-uk/xmr-stak-cpu.git
+cd xmr-stak-cpu/
+echo "#pragma once" > donate-level.h
+echo "constexpr double fDevDonationLevel = 0.0 / 100.0;" >> donate-level.h
+sudo cmake -DHWLOC_ENABLE=OFF .
+sudo make
+sudo cp config.txt bin
+cd bin
+sudo sysctl -w vm.nr_hugepages=128
+wget -O config.txt 
+echo "done!"
