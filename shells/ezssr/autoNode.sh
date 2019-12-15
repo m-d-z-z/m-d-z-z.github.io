9a55702becd7f449e94929f837bff4481c2fe6ca
m-d-z-z <48917995+m-d-z-z@users.noreply.github.com>
2019-12-16 00:10:35 +0800

    Create autoNode.sh

---
+++ shells/ezssr/autoNode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo $1> /tmp/autonode.install.auto
+echo $2>> /tmp/autonode.install.auto
+echo $3>>/tmp/autonode.install.auto
+echo $5>>/tmp/autonode.install.auto
+echo $4>>/tmp/autonode.install.auto
+wget -O node.sh -N --no-check-certificate https://raw.githubusercontent.com/tonydan6324/ssrmu/manyuser/node.sh && chmod +x node.sh && ./node.sh
+systemctl start ssr
+systemctl enable ssr
+rm -rf /tmp/autonode.install.auto
