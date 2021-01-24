2c57ac629586ad3dcf35c54789bbbd56bb81a8f8
m-d-z-z <48917995+m-d-z-z@users.noreply.github.com>
2021-01-24 16:39:57 +0800

    Update n2n_build_install.sh

--- shells/n2n/n2n_build_install.sh
+++ shells/n2n/n2n_build_install.sh
@@ -2,7 +2,7 @@
 if [ "`cat /etc/os-release | grep centos`" == '' ]; then
 apt-get install build-essential libssl-dev autoconf unzip -y
 else
-yum install cmake gcc gcc-c++ git wget openssl-devel autoconf unzip -y
+yum install cmake gcc gcc-c++ git wget openssl-devel autoconf automake unzip -y
 fi
 
 wget -O n2n.zip https://github.com/ntop/n2n/archive/2.8.zip
